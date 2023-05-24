3899bd7aa327a644cee85be80674bee838b85170
chuqq <chuqingq@qq.com>
2023-05-24 22:12:56 +0800

    引入assert

--- mmq_test.go
+++ mmq_test.go
@@ -1,71 +1,44 @@
 package mmq
 
 import (
+	"errors"
 	"log"
 	"os"
 	"testing"
 	"time"
 
 	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
 )
-
-var (
-	caCert      []byte
-	serverKey   []byte
-	serverCert  []byte
-	clientKey   []byte
-	clientCert  []byte
-	clientKey2  []byte
-	clientCert2 []byte
-	addr        string = "127.0.0.1:8080"
-)
-
-func initCert() {
-	var err error
-	// ca
-	caCert, err = os.ReadFile("certs/ca.cer")
-	assert(err == nil)
-	// server
-	serverKey, err = os.ReadFile("certs/server.key")
-	assert(err == nil)
-	serverCert, err = os.ReadFile("certs/server.cer")
-	assert(err == nil)
-	// client
-	clientKey, err = os.ReadFile("certs/client.key")
-	assert(err == nil)
-	clientCert, err = os.ReadFile("certs/client.cer")
-	assert(err == nil)
-	// invalid client
-	clientKey2, err = os.ReadFile("certs/another/client.key")
-	assert(err == nil)
-	clientCert2, err = os.ReadFile("certs/another/client.cer")
-	assert(err == nil)
-}
 
 func TestMmq(t *testing.T) {
+	assert := assert.New(t)
+
 	logger.SetLevel(logrus.DebugLevel)
-	initCert()
-	// 用例开始
-	// startServer:server1
+	initCert(assert)
+
+	// startServer
 	log.Printf("====StartServer")
 	s, err := NewServer(addr, serverKey, serverCert, caCert)
-	assert(err == nil)
+	assert.NoError(err)
+
 	// onStartServer
 	msg := *s.Recv()
 	log.Printf("s recv: %v", msg)
-	assert(msg.String("cmd") == "onStartServer")
-	assert(msg.Bool("success"))
+	assert.Equal(msg.String("cmd"), "onStartServer")
+	assert.True(msg.Bool("success"))
 	defer s.Close()
 
 	// client1
 	log.Printf("====startClient")
 	c1, err := NewClient(addr, clientKey, clientCert, caCert)
-	assert(err == nil)
+	assert.NoError(err)
+
 	// onStartClient
 	msg = *c1.Recv()
 	log.Printf("c1 recv: %v", msg)
-	assert(msg.String("cmd") == "onStartClient")
-	assert(msg.Bool("success"))
+	assert.Equal(msg.String("cmd"), "onStartClient")
+	assert.True(msg.Bool("success"))
 	defer c1.Close()
 
 	// msg = *s.Recv()
@@ -74,24 +47,24 @@ func TestMmq(t *testing.T) {
 	// client2
 	log.Printf("====startClient2")
 	c2, err := NewClient(addr, clientKey, clientCert, caCert)
-	assert(err == nil)
+	assert.NoError(err)
 	// defer c.StopClient("client2")
 	// onStartClient
 	msg = *c2.Recv()
 	log.Printf("c2 recv: %v", msg)
-	assert(msg.String("cmd") == "onStartClient")
-	assert(msg.Bool("success"))
+	assert.Equal(msg.String("cmd"), "onStartClient")
+	assert.True(msg.Bool("success"))
 	// assert(msg["client"].(string) == "client2")
 
 	// client3
 	log.Printf("====startClient3 invalid")
 	c3, err := NewClient(addr, clientKey2, clientCert2, caCert)
-	assert(err == nil)
+	assert.NoError(err)
 	// onStartClient
 	msg = *c3.Recv()
 	log.Printf("c3 recv: %v", msg)
-	assert(msg.String("cmd") == "onStartClient")
-	// assert(!msg.Bool("success"))
+	assert.Equal(msg.String("cmd"), "onStartClient")
+	assert.True(msg.Bool("success"))
 
 	// client1 subscribe
 	log.Printf("====client1 subscribe client1Topic")
@@ -105,22 +78,66 @@ func TestMmq(t *testing.T) {
 			"key5": "value5"
 		}
 	}`)
-	assert(err == nil)
+	assert.NoError(err)
 	msg = *pmsg
 	c2.Publish("client1Topic", &msg)
 
 	// client1 recv
 	msg = *c1.Recv()
 	log.Printf("c1 recv msg: %v", msg)
-	assert(msg.String("cmd") == "publish")
-	assert(msg.String("topic") == "client1Topic")
+	assert.Equal(msg.String("cmd"), "publish")
+	assert.Equal(msg.String("topic"), "client1Topic")
+
 	// stopClient:client2
 	log.Printf("====stopClient client2")
 	c2.Close()
 }
 
-func assert(b bool) {
-	if !b {
-		panic("assert failed!!!!")
-	}
+func TestAssert(t *testing.T) {
+	assert := assert.New(t)
+
+	err := errors.New("this is an error")
+	assert.NoError(err)
+
+	assert.Equal(1+2, 4)
+}
+
+var (
+	caCert      []byte
+	serverKey   []byte
+	serverCert  []byte
+	clientKey   []byte
+	clientCert  []byte
+	clientKey2  []byte
+	clientCert2 []byte
+	addr        string = "127.0.0.1:8080"
+)
+
+func initCert(assert *assert.Assertions) {
+	var err error
+
+	// ca
+	caCert, err = os.ReadFile("certs/ca.cer")
+	assert.NoError(err)
+
+	// server
+	serverKey, err = os.ReadFile("certs/server.key")
+	assert.NoError(err)
+
+	serverCert, err = os.ReadFile("certs/server.cer")
+	assert.NoError(err)
+
+	// client
+	clientKey, err = os.ReadFile("certs/client.key")
+	assert.NoError(err)
+
+	clientCert, err = os.ReadFile("certs/client.cer")
+	assert.NoError(err)
+
+	// invalid client
+	clientKey2, err = os.ReadFile("certs/another/client.key")
+	assert.NoError(err)
+
+	clientCert2, err = os.ReadFile("certs/another/client.cer")
+	assert.NoError(err)
 }
