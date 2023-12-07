8c21505758a0452aa907ca78b139d7ad4b64673a
chuqq <chuqingq@qq.com>
2023-12-08 00:45:32 +0800

    msg.go改为command.go

---
+++ v2/command.go
@@ -0,0 +1,25 @@
+package mmq
+
+import (
+	"bytes"
+	"encoding/gob"
+)
+
+type Command struct {
+	Cmd   string
+	Topic string
+	Data  []byte
+}
+
+func (s *Command) FromBytes(b []byte) error {
+	return gob.NewDecoder(bytes.NewReader(b)).Decode(s)
+}
+
+func (s *Command) ToBytes() ([]byte, error) {
+	var buf bytes.Buffer
+	err := gob.NewEncoder(&buf).Encode(s)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
